368ecadd86b67fc63e85521dc81def52264ad0f7
“uri” <uri@dtravel.com>
2022-03-26 20:32:37 +0700

    init project

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <p className="home-title">Home</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/settings/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Settings.scss'
+
+const Settings = () => {
+  return (
+    <div className="settingsContainer">
+      <p className="setting-title">Settings</p>
+    </div>
+  )
+}
+
+export default Settings
