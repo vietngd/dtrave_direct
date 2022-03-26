62655b9d8dc938d11eea5a09ada2c2a99b610b8e
“uri” <uri@dtravel.com>
2022-03-26 21:02:52 +0700

    change route

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.scss'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+  const gotoSetting = () => {
+    navigate('/setting')
+  }
   return (
     <div className="homeContainer">
       <p className="home-title">Home</p>
+      <button onClick={gotoSetting}>go to setting</button>
     </div>
   )
 }
--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Settings.scss'
+import { useNavigate } from 'react-router-dom'
 
 const Settings = () => {
+  const navigate = useNavigate()
+  const gotoHome = () => {
+    navigate('/')
+  }
   return (
     <div className="settingsContainer">
       <p className="setting-title">Settings</p>
+      <button onClick={gotoHome}>go to home</button>
     </div>
   )
 }
