556c0d9bb34e7a7c2faac42739be0f3d0a0b958f
vietngd <viet.ngd.2002@gmail.com>
2022-04-05 21:34:47 +0700

    Functions

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -3,29 +3,35 @@ import "./Header.scss";
 import { useNavigate } from "react-router-dom";
 import Logodtravel from "./Logodtravel.svg";
 const menu = [
-  { name: "Home", url: "/" },
-  { name: "Settings", url: "/settings" },
-  { name: "Reservations", url: "/reservations" }
-]
+  { name: "HOME", url: "/" },
+  { name: "LISTINGS", url: "/listing" },
+  { name: "RESERVATIONS", url: "/reservations" },
+];
 const Header = () => {
   const navigate = useNavigate();
   const gotoHome = (url) => {
     navigate(url);
   };
+
   return (
     <div className="headerContainer">
       <img className="img" src={Logodtravel} alt=""></img>
-      <ul>{menu.map((el, idx) => {
-        return (
-          <li className="headerItem" onClick={() => gotoHome(el.url)}>{el.name}</li>
-        )
-      })}</ul>
       <ul>
-        <li className="headerItem" onClick={() => gotoHome('/settings')}>Settings</li>
-        <li className="headerItem" onClick={() => gotoHome('/')}>Home</li>
-        <li className="headerItem" onClick={() => gotoHome('/')}>Home</li>
-        <li className="headerItem" onClick={() => gotoHome('/')}>Home</li>
-        <li className="headerItem" onClick={() => gotoHome('/')}>Home</li>
+        {menu.map((el, idx) => {
+          return (
+            <li className="headerItem" onClick={() => gotoHome(el.url)}>
+              {el.name}
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="distance">
+        <li className="headerItem" onClick={() => gotoHome("/discord")}>
+          DISCORD
+        </li>
+        <li className="headerItem" onClick={() => gotoHome("/twitter")}>
+          TWITTER
+        </li>
       </ul>
     </div>
   );
