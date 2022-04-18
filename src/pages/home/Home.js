54fbc1ef75babe9e427e3e9ac629aabcab0ac823
vietngd <viet.ngd.2002@gmail.com>
2022-04-18 21:21:54 +0700

    home_dtravel

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Home.scss";
 import Daux from "./Daux.svg";
+import Chamxanh from "./Chamxanh.svg";
+import Chamtrang from "./Chamtrang.svg";
+import Busd from "./Busd.svg";
+import Usdc from "./Usdc.svg";
+import Usdt from "./Usdt.png";
 const Home = () => {
   return (
     <div className="paper">
@@ -48,18 +53,7 @@ const Home = () => {
                 <button className="join"> Get help in Discord</button>
               </div>
             </div>
-            <div className="reservations">
-              <div className="w-full">
-                <p className="column">
-                  <span className="left-column">Listings</span>
-                  <span className="right-column"> Show all</span>
-                </p>
-                <p className="size">
-                  You don’t have any listings on Dtravel yet
-                </p>
-                <button className="join"> Import Listings</button>
-              </div>
-            </div>
+
             <div className="reservations">
               <div className="w-full">
                 <p className="column">
@@ -75,8 +69,43 @@ const Home = () => {
           </div>
 
           <div className="right-home">
-            <div className="compiled">
-              <p className="left-column">Getting Started</p>
+            <div className="dismiss">
+              <p className="compiled">
+                <span className="left-column">Getting Started</span>
+                <span className="right-column"> Show all</span>
+              </p>
+              <p className="wallet">
+                <img src={Chamxanh} alt=""></img>
+                <span className="network">Wallet connected</span>
+              </p>
+              <p className="wallet">
+                <img src={Chamxanh} alt=""></img>
+                <span className="network">Connect to Hostaway PMS</span>
+              </p>
+              <p className="wallet">
+                <img src={Chamtrang} alt=""></img>
+                <span className="network">Switch to BSC Network</span>
+              </p>
+            </div>
+
+            <div className="dismiss">
+              <p className="compiled">
+                <span className="left-column">Assets</span>
+              </p>
+              <p className="USD">
+                <span className="conected">USDT</span>
+                <img className="usdc" src={Usdt} alt=""></img>
+              </p>
+              <p className="USD">
+                <span className="conected">USDC</span>
+                <img className="usdc" src={Usdc} alt=""></img>
+              </p>
+              <p className="USD">
+                <span className="conected">BUSD</span>
+                <img className="usdc" src={Busd} alt=""></img>
+              </p>
+              <p>$12,324.29</p>
+              <p>Total USD value in your wallet</p>
             </div>
           </div>
         </div>
